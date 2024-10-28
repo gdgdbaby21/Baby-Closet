8644c26ffffee1854b23a80bda78710f2a6b1247
金釘早貴 <kanakugisaki@kanakugihayatakanonotobukkukonpyuta.local>
2024-10-28 15:37:03 +0900

    ログインと新規登録画面のフロントエンドの実装

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    date_joined = None
+    groups = None
+    user_permissions = None
+    
+    
+username = models.CharField(max_length=50, unique=True)
+email = models.EmailField(max_length=50, unique=True)
+created_at = models.DateTimeField(auto_now_add=True)
+updated_at = models.DateTimeField(auto_now=True)
+
+
+USERNAME_FIELD = "username"
+EMAIL_FIELD = "email"
+REQUIRED_FIELDS = ["email"]
+
+class Meta:
+    db_table = "users"
---
+++ app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from app.forms import SignupForm, LoginForm
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+
+
+
+class PortfolioView(View):
+    def get(self, request):
+        return render(request, "portfolio.html")
+
+class SignupView(View):
+    def get(self, request):
+        form = SignupForm() 
+        return render(request, "signup.html", context={
+            "form":form
+        })
+    def post(self, request):
+        print(request.POST)
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        return render(request, "signup.html", context={
+            "form": form
+        })
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+    def post(self, request):
+        print(request.POST)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            login(request, form.user)
+            return redirect("home")
+        return render(request, "login.html", context={
+            "form": form
+        })
+    
+       
+class HomeView(LoginRequiredMixin, View):
+    login_url = "login"
+    def get(self, request):
+        return render(request, "home.html")
